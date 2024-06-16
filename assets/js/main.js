cc1ba1b5f1d18ddeb50b6302743ad8246a1734d7
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-06-16 21:01:50 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,20 @@
 
 
 //navbar.js
-// Get the navbar and offset
 const navbar = document.querySelector('.navbar');
 const offset = navbar.offsetTop + navbar.offsetHeight;
 let lastScrollTop = 0;
+
+const mainContainer = document.querySelector('main');
 
 window.addEventListener('scroll', () => {
   const scrollTop = window.scrollY;
   if (scrollTop >= offset) {
     navbar.classList.add('sticky');
+    mainContainer.style.paddingTop = navbar.offsetHeight + 'px';
   } else {
     navbar.classList.remove('sticky');
+    mainContainer.style.paddingTop = 0;
   }
   if (scrollTop > lastScrollTop) {
     navbar.classList.add('hide');
@@ -21,6 +24,7 @@ window.addEventListener('scroll', () => {
   }
   lastScrollTop = scrollTop;
 });
+
 
 
 //---
